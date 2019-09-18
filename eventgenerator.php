f8e23a69691a4146b3bb6bfaefca1464489b5076
Shahid Shaikh <rwtc66@gmail.com>
2019-09-18 16:52:06 +0530

    Update eventgenerator.php

--- eventgenerator.php
+++ eventgenerator.php
@@ -12,10 +12,20 @@ add_action( 'post_updated', 'post_update_function', 10, 3 );
 function post_update_function( $post_ID, $post_after, $post_before ) {
 	// on each change, call this api to notify the content engine
 	$event = null;
-	if($post_before -> post_status === 'auto-draft') {
+	// if article is auto draft
+	if($post_before -> post_status === 'auto-draft' && $post_after -> post_status === 'publish') {
 		$event = 'new_article';
-	} else {
+	// if article is draft then published
+	} else if($post_before -> post_status === 'draft' && $post_after -> post_status === 'publish') {
+		$event = 'new_article';
+	// if its updated article
+	} else if($post_before -> post_status === 'publish' && $post_after -> post_status === 'publish') {
 		$event = 'updated_article';
+	} else {
+		return;
+	}
+	if($event === null) {
+		return;
 	}
 	$response = wp_remote_post('http://localhost:3000/api/event', array(
 		'method' => 'POST',
